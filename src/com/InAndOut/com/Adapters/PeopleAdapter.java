2658f7c02d980fa240dd626b7834fb30d48553b8
VicV <victorvuc@gmail.com>
2013-11-15 14:51:24 -0500

    added some color to the indicators

--- src/com/InAndOut/com/Adapters/PeopleAdapter.java
+++ src/com/InAndOut/com/Adapters/PeopleAdapter.java
@@ -1,20 +1,20 @@
 package com.InAndOut.com.Adapters;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.content.Context;
 import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.RelativeLayout;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.InAndOut.R;
 import com.InAndOut.Views.RoundedImageView;
 import com.parse.ParseUser;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class PeopleAdapter extends ArrayAdapter<ParseUser> {
 
@@ -39,10 +39,10 @@ public class PeopleAdapter extends ArrayAdapter<ParseUser> {
 			convertView = mInflater.inflate(R.layout.homescreen_item, null);
 			holder = new ContactViewHolder();
 			holder.name = (TextView) convertView.findViewById(R.id.name);
-			holder.thumbnail = (RoundedImageView) convertView
-					.findViewById(R.id.thumbnail);
+			holder.thumbnail = (RoundedImageView) convertView.findViewById(R.id.thumbnail);
 			holder.state = (TextView) convertView.findViewById(R.id.state);
 			holder.status = (TextView) convertView.findViewById(R.id.status);
+			holder.indicator = (LinearLayout) convertView.findViewById(R.id.indicator);
 
 			convertView.setTag(holder);
 		} else {
@@ -52,10 +52,12 @@ public class PeopleAdapter extends ArrayAdapter<ParseUser> {
 
 		if (isIn) {
 			holder.state.setText("in");
+			holder.indicator.setBackgroundColor(mContext.getResources().getColor(R.color.green));
 			holder.state.setTextColor(Color.parseColor("#12a976"));
 
 		} else {
 			holder.state.setText("Out");
+			holder.indicator.setBackgroundColor(mContext.getResources().getColor(R.color.yellow));
 			holder.status.setText("Busy");
 			holder.state.setTextColor(Color.parseColor("#FF0000"));
 		}
@@ -71,6 +73,7 @@ public class PeopleAdapter extends ArrayAdapter<ParseUser> {
 		} else {
 			holder.thumbnail.setImageResource(R.drawable.sylvia);
 		}
+		
 
 		return convertView;
 	}
@@ -81,4 +84,5 @@ class ContactViewHolder {
 	public TextView state;
 	public TextView status;
 	public RoundedImageView thumbnail;
+	public LinearLayout indicator;
 }
